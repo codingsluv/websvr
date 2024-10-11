546e556722bd01a60a73b71ce432c6b8783db362
MuhammadNur <delusionboys974@gmail.com>
2024-10-11 11:59:54 +0700

    route path params

---
+++ routes.js
@@ -0,0 +1,47 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/users/{username?}',
+        handler: (request, h) => {
+            const { username = 'orang-asing'} = request.params
+            return `Hello ${username}`;
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+ 
+module.exports = routes;
